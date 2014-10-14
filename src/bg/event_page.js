5f0ea087cf982537398e2a767c3ebdf060b3641f
jzlin <jerry.jzlin@gmail.com>
2014-10-14 22:34:54 +0800

    在事件頁面讀取 RSS
    在事件頁面讀取 RSS 並儲存置 local storage。

--- src/bg/event_page.js
+++ src/bg/event_page.js
@@ -1,1 +1,82 @@
-// event page
+
+(function () {
+
+chrome.commands.onCommand.addListener(function(command)
+{
+  console.debug('command is : ' + command);
+
+  // Develop use : reload extension.
+  if (command == 'reload_extension')
+    chrome.runtime.reload();
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  if (alarm.name === 'getFeed') {
+    console.log('hello: ' + new Date());
+    getFeed();
+  }
+});
+
+chrome.alarms.create('getFeed', {
+  when: new Date('2014-10-01').getTime(),
+  periodInMinutes: 1
+});
+
+google.load("feeds", "1");
+
+var storageInBG = storage.local;
+
+var feeds = [];
+
+function FeedLoader(params, callback) {
+  if (typeof(params) !== 'object' || 
+    typeof(params.q) === 'undefined') {
+    callback({
+      error: {
+        message: "params is undefined"
+      }
+    });
+  }
+
+  var feed = new google.feeds.Feed(params.q);
+  feed.setNumEntries(params.num || 10);
+  feed.load(function (data) {
+    // Parse data depending on the specified response format, default is JSON.
+    console.log(data);
+    if (typeof(callback) === 'function') {
+      callback(data);
+    }
+  });
+}
+
+function getFeed() {
+  storageInBG.get('feedList', function (data) {
+    var feedSources = [];
+    if (typeof(data) === "object" && typeof(data.length) !== "undefined") {
+      feedSources = data;
+    }
+    if (feeds.length === 0) {
+      for (var i = 0; i < feedSources.length; i++) {
+        FeedLoader({q: feedSources[i].url, num: 10}, function (data) {
+          if (typeof(data) !== 'object' ||
+            typeof(data.error) === 'object') {
+            console.error(data.error.message);
+          }
+          else {
+            var feed = data.feed;
+            for (var j = 0; j < feed.entries.length; j++) {
+              var publishedDate = new Date(feed.entries[j].publishedDate);
+              feed.entries[j].publishedDate = publishedDate.getTime();
+            }
+            feeds.push(feed);
+            if (feeds.length !== 0) {
+              storageInBG.set('feeds', feeds);
+            }
+          }
+        });
+      }
+    }
+  });
+}
+
+}());
