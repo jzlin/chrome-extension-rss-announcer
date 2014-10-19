a92b0aca206b0e3b7290fb811981b125b50886df
jzlin <jerry.jzlin@gmail.com>
2014-10-19 12:51:02 +0800

    加入 Google Analytics
    加入 Google Analytics

--- src/option/js/option.js
+++ src/option/js/option.js
@@ -185,3 +185,13 @@ optionModule.controller('AnnouncerSettingCtrl', [
 }]);
 
 }());
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-44540819-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
